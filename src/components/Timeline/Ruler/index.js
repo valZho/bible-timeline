1c142c38082abc928fac3e0d3532dfcd3488a931
ValZho <dd.github@inv.email>
2024-05-22 18:47:50 -0500

    ruler - in progress

--- src/components/Timeline/Ruler/index.js
+++ src/components/Timeline/Ruler/index.js
@@ -14,20 +14,61 @@ const Ruler = () => {
   // years to show ... add a little buffer on the right for event
   const totalYears = Math.ceil((farRight + 200) / scale) + 10;
 
-  const ticks = [];
-  for (let i = 1; i < totalYears; i++) {
-    // yearly ticks: only zoomed in above a certain scale
-    if (scale > 20) {
-      ticks.push(<div key={`tick${i}`} className="tick normal" style={{ width: scale }}></div>);
-    }
+  let ticks = [];
 
-    // shmita every 7 years
-    if (i % 7 === 0) {
-      ticks.push(<div key={`tick${i}`} className="tick shmita" style={{ width: scale }}></div>);
-    }
-  }
+  // NORMAL TICK VALUES:
+  // scale < 5
+  // hidden
 
-  return <div className="rulerContainer" style={{ width: farRight + 200 }}></div>;
+  // // scale > 4
+  // let scaledProps = {
+  //   className="tick normal"
+  //   width="2"
+  //   x=""
+  // }
+
+  // // generate all the tick vars before messing with (slow) jsx
+  // const tickData = Array(totalYears).fill(0).map((_, i) => {
+
+  // })
+
+  // const ticks = Array(totalYears)
+  //   .fill(0)
+  //   .map((_, i) => {
+  //     const x = i * scale;
+  //     return (
+  //       <>
+  //         <rect width="2" height="30%" y="70%" className={`tick yr${i + 1}`} x={x} />
+  //         <text x={x} y="60%" textAnchor="left" fontSize=".8rem">
+  //           {i + 1}
+  //         </text>
+  //       </>
+  //     );
+  //   });
+
+  const normalTicks = scale > 8 ? <rect x="0" y="0" width="100%" height="100%" fill="url(#NormalTicks)" /> : '';
+
+  return (
+    <div className="rulerContainer" style={{ width: farRight + 200 }}>
+      <svg version="1.1" className={`ruler scale${scale}`} width="100%" height="100%">
+        <defs>
+          <linearGradient id="SchmitaGradient" x1="0" x2="1" y1="0" y2="0">
+            <stop offset="50%" stopColor="lightgreen" stopOpacity="1" />
+            <stop offset="100%" stopColor="lightgreen" stopOpacity="0" />
+          </linearGradient>
+          <pattern id="NormalTicks" x="0" y="0" width={scale} height="100%" patternUnits="userSpaceOnUse">
+            <rect className="tickMark" x="0" y="80%" width="2" height="20%" fill="black" />
+          </pattern>
+          <pattern id="ShmitaTicks" x="0" y="0" width={scale * 7} height="100%" patternUnits="userSpaceOnUse">
+            <rect className="shmita" x={scale * 6} y="60%" width={scale} height="40%" fill="url(#SchmitaGradient)" />
+            <rect className="tickMark" x={scale * 6} y="60%" width="2" height="40%" fill="black" />
+          </pattern>
+        </defs>
+        {normalTicks}
+        <rect x="0" y="0" width="100%" height="100%" fill="url(#ShmitaTicks)" />
+      </svg>
+    </div>
+  );
 };
 
 export default Ruler;
